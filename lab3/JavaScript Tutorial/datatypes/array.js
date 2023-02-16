d0721927dfad2f4deadab7f2e75710d5b5cdff07
TheyCallMeTemikk <124165861+TheyCallMeTemikk@users.noreply.github.com>
2023-02-16 21:47:12 +0600

    Update array.js

--- lab3/JavaScript Tutorial/datatypes/array.js
+++ lab3/JavaScript Tutorial/datatypes/array.js
@@ -1,1 +1,83 @@
+// Task 1 :
+let fruits = ["Apples", "Pear", "Orange"];
+// push a new value into the copy
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+// what's in fruits
+alert(fruits.length); // 4
+// That’s because arrays are objects. So both shoppingCart and fruits are the references to the same array
+// Task 2
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[(styles.length - 1) / 2] = "Classics";
+alert( styles.shift() );// done
+styles.unshift("Rap", "Reggae");// done
+// Task 3:
+let arr = ["a", "b"];
 
+arr.push(function() {
+  alert( this );
+})
+
+arr[2](); // a,b,function(){...}
+
+//Task4:
+function sumInput() {
+
+    let numbers = [];
+  
+    while (true) {
+  
+      let value = prompt("A number please?", 0);
+  
+      // should we cancel
+      if (value === "" || value === null || !isFinite(value)) break;
+  
+      numbers.push(+value);
+    }
+  
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+  
+alert(sumInput());
+// Task5 :
+// Slow :
+function getMaxSubSum(arr) {
+    let maxSum = 0; // if we take no elements, zero will be returned
+    for (let i = 0; i < arr.length; i++) {
+      let sumFixedStart = 0;
+      for (let j = i; j < arr.length; j++) {
+        sumFixedStart += arr[j];
+        maxSum = Math.max(maxSum, sumFixedStart);
+      }
+    }
+    return maxSum;
+  }
+  alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+  alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+  alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+  alert( getMaxSubSum([1, 2, 3]) ); // 6
+  alert(getMaxSubSum([100, -9, 2, -3, 5])); // 100
+// FAST :
+function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+    for (let item of arr) { // for each item of arr
+      partialSum += item; // add it to partialSum
+      maxSum = Math.max(maxSum, partialSum); // remember the maximum
+      if (partialSum < 0) partialSum = 0; // zero if negative
+    }
+  
+    return maxSum;
+  }
+  
+  alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+  alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+  alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+  alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+  alert( getMaxSubSum([1, 2, 3]) ); // 6
+  alert( getMaxSubSum([-1, -2, -3]) ); // 0
